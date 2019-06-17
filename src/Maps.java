16cf6efc10346f01f51ca16d685d48537ea69070
navinaa <navinaat159@gmail.com>
2019-06-17 12:46:15 +0530

    comitted

---
+++ src/Maps.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class Maps
+{
+    public static void main(String[] args)
+    {
+        Map m1=new HashMap();
+        m1.put("1","navi");
+        m1.put(2,"rajee");
+        m1.put(3,"thamizh");
+        m1.put(4,"kishore");
+        m1.put("null","null");
+
+        System.out.println(m1.containsValue("navi"));
+        System.out.println(m1.containsKey(3));
+        System.out.println(m1.containsValue("nav"));
+        System.out.println(m1.remove("navi"));
+        System.out.println(m1.entrySet());
+        System.out.println(m1.get(10));
+        System.out.println(m1.keySet());
+
+
+    }
+}
